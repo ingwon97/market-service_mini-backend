5048159c0189f14b5db7c80a46cda83681b3027f
01192mg <01192mg@gmail.com>
2022-08-12 16:24:04 +0900

    Initial commit

---
+++ src/main/java/com/example/carrot/CarrotApplication.java
@@ -0,0 +1,19 @@
+package com.example.carrot;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class CarrotApplication {
+
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.properties,"
+            + "classpath:aws.yml";
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(CarrotApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
+    }
+
+}
