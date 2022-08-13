f32b7ba7989b66d6dd720c391616c010ad204fef
jack <jachky1205@gmail.com>
2022-08-13 11:05:17 +0900

    Revert "test"
    This reverts commit a82ab62e01e5b52de48ed3067f72b9ecd4dcdc59.

--- src/main/java/com/example/carrot/CarrotApplication.java
+++ src/main/java/com/example/carrot/CarrotApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-
 @EnableJpaAuditing
 @SpringBootApplication
 public class CarrotApplication {
