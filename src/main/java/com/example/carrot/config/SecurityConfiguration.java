db9035897907c4f98d05b6987eb152cb146b81ab
sunho <yalla12@naver.com>
2022-08-16 17:39:13 +0900

    sunho branch revert selected changes

---
+++ src/main/java/com/example/carrot/config/SecurityConfiguration.java
@@ -0,0 +1,69 @@
+package com.example.carrot.config;
+
+import com.example.carrot.jwt.AccessDeniedHandlerException;
+import com.example.carrot.jwt.AuthenticationEntryPointException;
+import com.example.carrot.jwt.TokenProvider;
+import com.example.carrot.service.MemberDetailsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@ConditionalOnDefaultWebSecurity
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+public class SecurityConfiguration {
+
+  @Value("${jwt.secret}")
+  String SECRET_KEY;
+  private final TokenProvider tokenProvider;
+  private final MemberDetailsService userDetailsService;
+  private final AuthenticationEntryPointException authenticationEntryPointException;
+  private final AccessDeniedHandlerException accessDeniedHandlerException;
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  @Order(SecurityProperties.BASIC_AUTH_ORDER)
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.cors();
+
+    http.csrf().disable()
+
+            .exceptionHandling()
+            .authenticationEntryPoint(authenticationEntryPointException)
+            .accessDeniedHandler(accessDeniedHandlerException)
+
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+            .and()
+            .authorizeRequests()
+            .antMatchers("/api/members/**").permitAll()
+            .antMatchers("/api/post/**").permitAll()
+            .antMatchers("/api/comment/**").permitAll()
+            .antMatchers("/api/reply/**").permitAll()
+            .anyRequest().authenticated()
+
+            .and()
+            .apply(new JwtSecurityConfiguration(SECRET_KEY, tokenProvider, userDetailsService));
+
+    return http.build();
+  }
+}
