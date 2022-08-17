7b7e21d82487a973bb602f2acfe401462dc9a7df
01192mg <01192mg@gmail.com>
2022-08-18 00:02:33 +0900

    Comment Controller, Service 코드 수정

--- src/main/java/com/example/carrot/service/CommentService.java
+++ src/main/java/com/example/carrot/service/CommentService.java
@@ -1,13 +1,12 @@
 package com.example.carrot.service;
 
-import com.example.carrot.jwt.TokenProvider;
 import com.example.carrot.model.Comment;
 import com.example.carrot.model.Member;
 import com.example.carrot.model.Post;
 import com.example.carrot.repository.CommentRepository;
 import com.example.carrot.repository.MemberRepository;
 import com.example.carrot.repository.PostRepository;
-import com.example.carrot.response.CommentRequestDto;
+import com.example.carrot.request.CommentRequestDto;
 import com.example.carrot.response.CommentResponseDto;
 import com.example.carrot.response.ResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -15,10 +14,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
@@ -28,47 +25,14 @@ public class CommentService {
     private final MemberRepository memberRepository;
     private final PostRepository postRepository;
     private final CommentRepository commentRepository;
-    private final TokenProvider tokenProvider;
 
     @Transactional
-    public ResponseDto<?> createComment(Long postId, CommentRequestDto requestDto, HttpServletRequest request) {
-
-        if (null == request.getHeader("Refresh-Token")) {
-            return ResponseDto.fail("MEMBER_NOT_FOUND",
-                    "로그인이 필요합니다.");
-        }
-
-        if (null == request.getHeader("Authorization")) {
-            return ResponseDto.fail("MEMBER_NOT_FOUND",
-                    "로그인이 필요합니다.");
-        }
-
-        Member member = validateMember(request);
-        if (null == member) {
-            return ResponseDto.fail("INVALID_TOKEN", "Token이 유효하지 않습니다.");
-        }
-        Post post = isPresentPost(postId);
-        if (null == post) {
-            return ResponseDto.fail("NOT_FOUND", "존재하지 않는 게시글 id 입니다.");
-        }
-
+    public ResponseDto<?> createComment(Long postId, CommentRequestDto requestDto, UserDetails userDetails) {
+        Member member = getMember(userDetails);
+        Post post = getPost(postId);
         Comment comment = new Comment(post, member, requestDto.getContent());
         Comment savedComment = commentRepository.save(comment);
         return ResponseDto.success(new CommentResponseDto(savedComment));
-    }
-
-    @Transactional(readOnly = true)
-    public Post isPresentPost(Long postId) {
-        Optional<Post> optionalPost = postRepository.findById(postId);
-        return optionalPost.orElse(null);
-    }
-
-    @Transactional
-    public Member validateMember(HttpServletRequest request) {
-        if (!tokenProvider.validateToken(request.getHeader("Refresh-Token"))) {
-            return null;
-        }
-        return tokenProvider.getMemberFromAuthentication();
     }
 
     public ResponseDto<?> findAllComment(Long postId) {
@@ -79,79 +43,26 @@ public class CommentService {
     }
 
     @Transactional
-    public ResponseDto<?> updateComment(Long commentId, CommentRequestDto requestDto, HttpServletRequest request) {
-        if (null == request.getHeader("Refresh-Token")) {
-            return ResponseDto.fail("MEMBER_NOT_FOUND",
-                    "로그인이 필요합니다.");
-        }
-
-        if (null == request.getHeader("Authorization")) {
-            return ResponseDto.fail("MEMBER_NOT_FOUND",
-                    "로그인이 필요합니다.");
-        }
-
-        Member member = validateMember(request);
-        if (null == member) {
-            return ResponseDto.fail("INVALID_TOKEN", "Token이 유효하지 않습니다.");
-        }
-
-        Comment comment = isPresentComment(commentId);
-
-        if (null == comment) {
-            return ResponseDto.fail("NOT_FOUND", "존재하지 않는 댓글 id 입니다.");
-        }
-
-        if (comment.validateMember(member)) {
-            return ResponseDto.fail("BAD_REQUEST", "작성자만 수정할 수 있습니다.");
-        }
-
-        validateAuthor(member, comment);
-
-        comment.update(requestDto.getContent());
-        return ResponseDto.success(comment);
-    }
-
-    private Comment isPresentComment(Long commentId) {
-
-        Optional<Comment> OptionalComment = commentRepository.findById(commentId);
-        return OptionalComment.orElse(null);
-
+    public ResponseDto<?> updateComment(Long commentId, CommentRequestDto requestDto, UserDetails userDetails) {
+        Member member = getMember(userDetails);
+        Comment findComment = getComment(commentId);
+        validateAuthor(member, findComment);
+        findComment.update(requestDto.getContent());
+        return ResponseDto.success(new CommentResponseDto(findComment));
     }
 
     @Transactional
-    public ResponseDto<?> deleteComment(Long commentId, HttpServletRequest request) {
-        if (null == request.getHeader("Refresh-Token")) {
-            return ResponseDto.fail("MEMBER_NOT_FOUND",
-                    "로그인이 필요합니다.");
-        }
-
-        if (null == request.getHeader("Authorization")) {
-            return ResponseDto.fail("MEMBER_NOT_FOUND",
-                    "로그인이 필요합니다.");
-        }
-
-        Member member = validateMember(request);
-        if (null == member) {
-            return ResponseDto.fail("INVALID_TOKEN", "Token이 유효하지 않습니다.");
-        }
-
-        Comment comment = isPresentComment(commentId);
-
-        if (null == comment) {
-            return ResponseDto.fail("NOT_FOUND", "존재하지 않는 댓글 id 입니다.");
-        }
-
-        if (comment.validateMember(member)) {
-            return ResponseDto.fail("BAD_REQUEST", "작성자만 수정할 수 있습니다.");
-        }
-
-        commentRepository.delete(comment);
+    public ResponseDto<?> deleteComment(Long commentId, UserDetails details) {
+        Member member = getMember(details);
+        Comment findComment = getComment(commentId);
+        validateAuthor(member, findComment);
+        commentRepository.delete(findComment);
         return ResponseDto.success(null);
     }
 
     private static void validateAuthor(Member member, Comment findComment) {
         if (!Objects.equals(findComment.getMember().getMember_id(), member.getMember_id())) {
-            throw new IllegalArgumentException("다른 사람의 댓글은 삭제할 수 없습니다");
+            throw new IllegalArgumentException("다른 사람의 댓글은 수정 또는 삭제할 수 없습니다");
         }
     }
 
